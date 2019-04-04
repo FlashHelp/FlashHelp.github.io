5e01a9a59db337fa5da5ee49a42ee3bb884a8f44
FlashHelp <48831724+FlashHelp@users.noreply.github.com>
2019-04-04 15:00:52 +0800

    头部自适应屏幕

---
+++ assets/js/main.js
@@ -0,0 +1,18 @@
+var w = window.innerWidth ||
+	document.documentElement.clientWidth ||
+	document.body.clientWidth;
+
+var h = window.innerHeight ||
+	document.documentElement.clientHeight ||
+	document.body.clientHeight;
+
+document.getElementById("topBox").style.height = h-10 + "px";
+
+window.onload = function() {
+	load_home();
+}
+
+function load_home() {
+	document.getElementById("iconsView").innerHTML =
+		'<object type="text/html" data="https://boxicons.com" width="100%" height="100%"></object>';
+}
